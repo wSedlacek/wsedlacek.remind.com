dbc65837c3e470265309b9081f95fcd4b244a7a2
wSedlacek <wsedlacekc@gmail.com>
2018-04-12 23:06:54 -0500

    Fix build.
    Toolbar no longer exist.

--- android/feature/src/main/java/com/remind/wsedlacek/forgetmenot/feature/SetupRemind.java
+++ android/feature/src/main/java/com/remind/wsedlacek/forgetmenot/feature/SetupRemind.java
@@ -13,8 +13,6 @@ public class SetupRemind extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
