5af2b1d305e9c8107e2d466a4c1e6211e6fca9db
wSedlacek <wsedlacekc@gmail.com>
2018-04-13 19:11:13 -0500

    Animated Background for Setup
    Todo:
    Create Ripple on Buttons.
    Create Animation for FAB.
    Create Animation for Navigation to Buzz
    Adjust background amiation to use ripple?

--- android/feature/src/main/java/com/remind/wsedlacek/forgetmenot/feature/Setup.java
+++ android/feature/src/main/java/com/remind/wsedlacek/forgetmenot/feature/Setup.java
@@ -3,12 +3,15 @@ package com.remind.wsedlacek.forgetmenot.feature;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Layout;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -20,7 +23,10 @@ public class Setup extends AppCompatActivity {
 
     final Context mContext = this;
 
-    ArrayList<View> mViews=new ArrayList<View>();
+    TransitionDrawable mBackground;
+
+    ArrayList<View> mViews = new ArrayList<View>();
+    View mCurrentView;
     View mEventOptions;
     View mNewEvent;
     View mExistingEvent;
@@ -33,6 +39,9 @@ public class Setup extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
 
+        View tBackground = (View) findViewById(R.id.background);
+        mBackground = (TransitionDrawable) tBackground.getBackground();
+        
         mEventOptions = (View) findViewById(R.id.event_options);
         mNewEvent = (View) findViewById(R.id.new_event);
         mExistingEvent = (View) findViewById(R.id.existing_event);
@@ -40,6 +49,8 @@ public class Setup extends AppCompatActivity {
         mExistingEventBtn = (Button) findViewById(R.id.existing_event_btn);
         mFAB = (FloatingActionButton) findViewById(R.id.fab);
         mViews.addAll(Arrays.asList(mEventOptions, mNewEvent, mExistingEvent));
+
+        updateView(mEventOptions, false, false);
 
 
         mNewEventBtn.setOnClickListener(new View.OnClickListener() {
@@ -76,20 +87,47 @@ public class Setup extends AppCompatActivity {
     //Intercept Back Button
     @Override
     public void onBackPressed() {
-        if (mEventOptions.getVisibility() == View.GONE) {
-            updateView(mEventOptions, false);
+        if (mEventOptions != mCurrentView) {
+            this.updateView(mEventOptions, false, true, true);
         } else {
             super.onBackPressed();
         }
     }
 
+    public void updateView(View tViewToUse) {
+        updateView(tViewToUse, false);
+    }
+
     public void updateView(View tViewToUse, Boolean tUseFAB) {
+        updateView(tViewToUse, tUseFAB, true);
+    }
+
+    public void updateView(View tViewToUse, Boolean tUseFAB, Boolean tAnimate) {
+        updateView(tViewToUse, tUseFAB, tAnimate, false);
+    }
+
+    public void updateView(View tViewToUse, Boolean tUseFAB, Boolean tAnimate, Boolean tInverseAnimation) {
         for (View tView: mViews) {
             tView.setVisibility(View.GONE);
         }
 
-        tViewToUse.setVisibility(View.VISIBLE);
+        mCurrentView = tViewToUse;
+        mCurrentView.setVisibility(View.VISIBLE);
         mFAB.setVisibility(tUseFAB ? View.VISIBLE : View.GONE);
+        if (tAnimate) updateBackground(tInverseAnimation);
+    }
+
+
+    public void updateBackground () {
+        updateBackground(false);
+    }
+
+    public void updateBackground (Boolean tInverseAnimation) {
+        if (tInverseAnimation) {
+            mBackground.reverseTransition(1000);
+        } else {
+            mBackground.startTransition(1000);
+        }
     }
 
 }
